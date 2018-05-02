f029ee98a09c2f6995141a4e918bbc81ebd8688d
Rodnorm <r.normando@hotmail.com>
2018-05-02 11:29:06 -0300

    :alien:
    login feito e tela de venda/locação tbm

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+
+ $(function () {
+                $("#datetimepicker_de").datetimepicker({
+                    defaultDate: "01/01/2018",
+                    locale: "pt-br",
+                });
+				$("#datetimepicker_ate").datetimepicker({
+                    defaultDate: "12/12/2018",
+                    locale: "pt-br",
+                });
+				$("#datetimepicker").datetimepicker({
+                    defaultDate: "01/01/2018",
+                    locale: "pt-br",
+                });
+				
+            });
